ac4ff322acc00fe5872a5b797de46f558092a59f
Михаил Созонтов <sozontovmisha@mail.ru>
2022-10-06 12:59:56 +0300

    fix+add image button+function add building

--- app/src/main/java/com/sozmi/dispetcher/MainActivity.java
+++ app/src/main/java/com/sozmi/dispetcher/MainActivity.java
@@ -4,10 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.PlacemarkMapObject;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageButton add_building;
+    Button cancel_building;
+    FrameLayout panel_building;
+    PlacemarkMapObject mapObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +27,22 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fTrans = getSupportFragmentManager().beginTransaction();
         fTrans.replace(R.id.fragment_view, mapFragment);
         fTrans.commit();
+        add_building  = findViewById(R.id.add_button);
+        cancel_building  = findViewById(R.id.cancel_button);
+        panel_building  = findViewById(R.id.panel_build);
+        add_building.setOnClickListener(view ->AddBuilding());
+        cancel_building.setOnClickListener(view ->CancelBuilding());
+    }
+
+    private void AddBuilding(){
+        mapObject =Map.addMarker(new Point(0,0),TypeBuilding.none);
+        add_building.setVisibility(View.INVISIBLE);
+        panel_building.setVisibility(View.VISIBLE);
+    }
+
+    private void CancelBuilding(){
+        Map.delMarker(mapObject);
+        add_building.setVisibility(View.VISIBLE);
+        panel_building.setVisibility(View.INVISIBLE);
     }
 }
--- app/src/main/java/com/sozmi/dispetcher/Map.java
+++ app/src/main/java/com/sozmi/dispetcher/Map.java
@@ -2,6 +2,7 @@ package com.sozmi.dispetcher;
 import static com.yandex.runtime.Runtime.getApplicationContext;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
@@ -13,13 +14,20 @@ import androidx.core.graphics.drawable.DrawableCompat;
 import com.yandex.mapkit.Animation;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.logo.Alignment;
+import com.yandex.mapkit.logo.HorizontalAlignment;
+import com.yandex.mapkit.logo.VerticalAlignment;
 import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.map.IconSet;
+import com.yandex.mapkit.map.IconStyle;
+import com.yandex.mapkit.map.PlacemarkMapObject;
 import com.yandex.mapkit.mapview.MapView;
 import com.yandex.runtime.image.ImageProvider;
 
 public final class Map {
     private static boolean isInit=false;
     private static MapView mapView;
+    private static com.yandex.mapkit.map.Map map;
 
     public static void setApi(){
         if(!isInit){
@@ -28,14 +36,12 @@ public final class Map {
         }
     }
 
-    public static com.yandex.mapkit.map.Map getMap(){
-        return mapView.getMap();
-    }
-    public static MapView getMapView(){
-        return mapView;
-    }
     public static void setMapView(View view){
         mapView = view.findViewById(R.id.mapview);
+        map=mapView.getMap();
+    }
+    public static void setLogoPosition(){
+        map.getLogo().setAlignment(new Alignment(HorizontalAlignment.LEFT, VerticalAlignment.BOTTOM));
     }
 
     public static void init(Context context){
@@ -44,27 +50,37 @@ public final class Map {
 
 
     public static void moveTo( Point coordinate){
-        getMap().move(
+        map.move(
                 new CameraPosition(coordinate, 11.0f, 0.0f, 0.0f),
                 new Animation(Animation.Type.SMOOTH, 0),
                 null);
     }
 
-    public static void addMarker(Point coordinate, TypeBuilding type){
-        Bitmap btm = null;
+    public static PlacemarkMapObject addMarker(Point coordinate, TypeBuilding type){
+        int id=0;
         switch (type){
             case police:
-                btm =getBitmapFromVectorDrawable(getApplicationContext(),R.drawable.ic_police);
+                id =R.drawable.ic_police;
                 break;
             case hospital:
-                btm =getBitmapFromVectorDrawable(getApplicationContext(),R.drawable.ic_hospital);
+                id =R.drawable.ic_hospital;
                 break;
             case fire_station:
-                btm =getBitmapFromVectorDrawable(getApplicationContext(),R.drawable.ic_firemen);
+                id =R.drawable.ic_firemen;
+                break;
+            case none:
+                id=R.drawable.ic_map_marker;
                 break;
         }
-        var pm =mapView.getMap().getMapObjects().addPlacemark(coordinate);
+        var btm = getBitmapFromVectorDrawable(getApplicationContext(),id);
+        var pm =map.getMapObjects().addPlacemark(coordinate);
         pm.setIcon(ImageProvider.fromBitmap(btm));
+        if(type==TypeBuilding.none)
+            pm.setDraggable(true);
+        return pm;
+    }
+    public static void delMarker(PlacemarkMapObject mapObject){
+       mapObject.getParent().remove(mapObject);
     }
 
     private static Bitmap getBitmapFromVectorDrawable(Context context, int drawableId) {
@@ -79,5 +95,14 @@ public final class Map {
         drawable.draw(canvas);
 
         return bitmap;
+    }
+    public static void OnStart(){
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
+    }
+
+    public static void OnStop(){
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
     }
 }
